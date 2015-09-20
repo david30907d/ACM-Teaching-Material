c747ff13aa604c34cdaa7ab23fe020978c92e8d4
david30907d <davidtnfsh@gmail.com>
2015-09-20 14:27:51 +0800

    新增教材

---
+++ 第一堂課9-22/C++公版.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+#include<stdio.h>
+using namespace std;
+int main(){
+	
+	while(scanf("%d",&n)!=EOF){
+		
+	}
+	return 0;	
+} 
---
+++ 第一堂課9-22/JAVA公版.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main{		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main m=new Main();
+		m.begin();
+		
+	}
+	public void begin(){		
+		while(cin.hasNext()){			
+			
+//=====================程式碼寫在這裡=========================
+
+		}
+		cin.close();
+	}	
+}
