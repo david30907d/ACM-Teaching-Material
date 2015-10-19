7c66e6cb627f01607d64241f54b391d877cca284
david <davidtnfsh@gmail.com>
2015-10-19 15:28:58 +0800

    新增註解

--- 2015-09-29/if else exercise.cpp
+++ 2015-09-29/if else exercise.cpp
@@ -4,14 +4,24 @@
 using namespace std;
 int main(){
     int input=0;
+    //int 是宣告型態
     scanf("%d",&input);
+    //scanf是從鍵盤輸入，%d代表要輸入十進位的數
+    //&input代表是要輸入到input這個變數
+
+
+    //if() else if(),這類的語法，需要小括號裡頭的判斷式是true,才會執行裏面的程式
+    //但是if 、else if 、else這些都是只會執行某一個，不可能都執行，若前面所有定義的條件都不符合就會執行else
     if(input%2==0){
+        //input %2  == 0 代表可以被二所整除，%代表除已某個數後所剩下的餘數
+        //所以若於零代表是二的倍數
         printf("input is even\n");
     }
     else if(input%3==0){
         printf("input is the mutiple of 3\n");
     }
     if(input%3==0 && input%2==0){
+        //&&代表and,要左邊跟右邊的條件都達成才會去執行內部程式，所以就是六的倍數
         printf("input is the mutiple of 6\n");
     }
     else{
@@ -19,3 +29,7 @@ int main(){
     }
 	return 0;
 }
+
+
+
+
