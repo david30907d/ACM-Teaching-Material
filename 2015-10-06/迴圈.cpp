b608bb2bf2048edea51b8779a011e7dde867959b
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2015-10-19 15:39:35 +0800

    新增for loop的註解

--- 2015-10-06/迴圈.cpp
+++ 2015-10-06/迴圈.cpp
@@ -3,8 +3,13 @@
 
 using namespace std;
 int fab(int num);
+//函式：會宣告函式的回傳值 函式名稱 及函式的參數
+//函式真正的定義會再最下方
 int main(){
 	for(int i=1;i<=9;++i){
+		//for()裏面和if一樣,需要判斷式為true才會繼續執行
+		//上面代表從1開始,每次都會檢查是不是有<=9,若有則執行回圈裡的程式
+		//執行完再執行++i,也就是i=i+1的縮寫
 		printf("%d\n",i);
 	}
 	int result=fab(10);
@@ -17,6 +22,8 @@ int main(){
 	return 0;
 }
 int fab(int num){
+	//函式真正的定義,因為有宣告函式為int型態
+	//所以就要回傳一個int的值,語法為return
 	if(num>0){
 		return fab(num-1)*num;
 	}
