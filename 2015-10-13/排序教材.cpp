2966e6292ab1c332e66ef80aa2bb25cba6376f2d
david <davidtnfsh@gmail.com>
2015-11-17 16:59:30 +0800

    修改一下變數名稱

--- 2015-10-13/排序教材.cpp
+++ 2015-10-13/排序教材.cpp
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <algorithm>
 using namespace std;
-int main(){	
-	int array[10]={};
-	int Alarr[10]={};
+int main(){
+	int array[50]={};
+	int Alarr[50]={};
 	int num=0;
-	while(scanf("%d",&num)!=EOF){		
+	while(scanf("%d",&num)!=EOF){
 		for(int i=0;i<num;++i){
 			scanf("%d",&array[i]);
 		}
@@ -16,29 +16,29 @@ int main(){
 		}
 		printf("=================\n");
 		//selection sort
-		
+
 		for(int i=0;i<num;++i){
-			int max=i;
+			int min=i;
 			for(int j=i+1;j<num;++j){
-				if(array[max]>array[j]){
-					max=j;					
-				}				
+				if(array[j]<array[min]){
+					min=j;
+				}
 			}
-			swap(array[max],array[i]);
+			swap(array[min],array[i]);
 		}
-		 
+
 		for(int i=0;i<num;++i){
 			printf("%d\n",array[i]);
 		}
 		printf("=================\n");
-		printf("請輸入num個數字");
+		printf("請輸入%d個數字",num);
 		for(int i=0;i<num;++i){
 			scanf("%d",&Alarr[i]);
 		}
-		sort(Alarr,Alarr+num);//從第一位排到最後一位+1 
+		sort(Alarr,Alarr+num);//從第一位排到最後一位+1
 		for(int i=0;i<num;++i){
 			printf("%d\n",Alarr[i]);
 		}
 	}
-	return 0;	
-} 
+	return 0;
+}
